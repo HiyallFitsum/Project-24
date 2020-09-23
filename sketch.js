d2a40bfb7405d49e54753f800501a86b70f0d7de
HiyallFitsum <70286522+HiyallFitsum@users.noreply.github.com>
2020-09-23 07:28:07 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -18,13 +18,13 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	targetSprite1=createSprite(650, height-45, 200, 20);
+	targetSprite1=createSprite(1000, height-45, 200, 20);
 	targetSprite1.shapeColor = "grey";
 
-	targetSprite2=createSprite(550, height-85, 20, 100);
+	targetSprite2=createSprite(900, height-85, 20, 100);
 	targetSprite2.shapeColor = "grey";
 
-	targetSprite3=createSprite(750, height-85, 20, 100);
+	targetSprite3=createSprite(1100, height-85, 20, 100);
 	targetSprite3.shapeColor = "grey";
 
 	groundSprite=createSprite(width/2, height-35, width,10);
@@ -34,13 +34,14 @@ function setup() {
 	paper = new Paper(100, 600);
 	//ground = new Ground(400, height, 750, 50);
 
-	targetBody1 = Bodies.rectangle(650, height-60, 200, 20, {isStatic:true});
+	targetBody1 = Bodies.rectangle(1000, height-60, 200, 20, {isStatic:true});
 	World.add(world, targetBody1);
 
-	targetBody2 = Bodies.rectangle(550 , height-85, 20, 100, {isStatic:true});
+	targetBody2 = Bodies.rectangle(900 , height-85, 20, 100, {isStatic:true});
 	World.add(world, targetBody2);
 
-	targetBody3 = Bodies.rectangle(750, height-85, 20, 100, {isStatic:true});
+	targetBody3 = Bodies.rectangle(1100, height-85, 20, 100, {isStatic:true});
+	World.add(world, targetBody3);
 
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
 	 World.add(world, ground);
@@ -58,7 +59,6 @@ function draw() {
   paper.display();
   //ground.display();
 
-  keyPressed();
 
   drawSprites();
  
@@ -68,7 +68,7 @@ function keyPressed(){
 
 if(keyCode === UP_ARROW){
 
-	Matter.Body.applyForce(paper.body, paper.body.position,{x:85, y:-85});
+	Matter.Body.applyForce(paper.body, paper.body.position,{x:80, y:-80});
 	
 }
 
